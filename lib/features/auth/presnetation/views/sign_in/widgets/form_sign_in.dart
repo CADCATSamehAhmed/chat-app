ecb09fbddb8ac045b42c389de9d3fcd9b45f7869
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-10-20 18:05:37 +0300

    add form sign

--- lib/features/auth/presnetation/views/sign_in/widgets/form_sign_in.dart
+++ lib/features/auth/presnetation/views/sign_in/widgets/form_sign_in.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mini_chat/core/shared_widgets/form_field.dart';
 
+Form buildSignInForm({
+  required BuildContext context,
+  required dynamic email,
+  required dynamic password,
+  required bool isObscure,
+  void Function()? onTap,
+  required String? Function(String? v)? emailValidate,
+  required String? Function(String? v)? passValidate,
+}) {
+  return Form(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        defaultTextForm(
+          label: 'enter your email',
+          textInputType: TextInputType.emailAddress,
+          controller: email,
+          iconData: Icons.email,
+          onValidate: emailValidate,
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.03,
+          width: MediaQuery.of(context).size.width * 0.03,
+        ),
+        defaultTextForm(
+          label: 'enter your password',
+          onValidate: passValidate,
+          textInputType: TextInputType.text,
+          controller: password,
+          iconData: Icons.lock,
+          obscureText: isObscure,
+          onTap: onTap,
+          isSuffix: true,
+        ),
+      ],
+    ),
+  );
+}
