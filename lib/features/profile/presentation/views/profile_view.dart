d4b9706a9b69800900aa980a67ceaf4fb78324b6
Basmal-Mustafa <basmalamustafa26@gmail.com>
2024-10-25 22:42:26 +0300

    save profile files

---
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:chat_app/features/profile/presentation/views/profile_body.dart';
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ProfileBody(),
+    );
+  }
+}
