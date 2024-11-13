5187d08d9e0916373aeea2fa846805074ca8576a
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2024-11-14 01:06:28 +0200

    tried to handle the previews error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'features/splash/presentation/views/splash_view.dart';
 
 void main() async {
