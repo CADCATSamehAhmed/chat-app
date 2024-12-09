7e8dcba4a9e4f7989672b2f76fd943fa5605767e
Moffa854 <mustafamoffa@gmail.com>
2024-12-09 21:58:20 +0200

    face_search

--- lib/features/home/presentation/view_model/home_states.dart
+++ lib/features/home/presentation/view_model/home_states.dart
@@ -12,3 +12,14 @@ class LogOutError extends HomeStates {
   final String error;
   LogOutError({required this.error});
 }
+
+class GetAllUsersLoading extends HomeStates {}
+
+class GetAllUsersSuccess extends HomeStates {}
+
+class GetAllUsersError extends HomeStates {
+  final String error;
+  GetAllUsersError({required this.error});
+}
+
+class SearchUsersState extends HomeStates {}
--- lib/features/home/presentation/views/widgets/search_body.dart
+++ lib/features/home/presentation/views/widgets/search_body.dart
@@ -2,51 +2,68 @@ import 'package:chat_app/core/themes/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CustomSearch extends SearchDelegate{
-  List<String> elements= ['1','2','3'];
+class CustomSearch extends SearchDelegate {
+  final List<String> searchItems = [
+    'Recent Chats',
+    'Online Users',
+    'Groups',
+    'Settings',
+    'Profile',
+    'Notifications'
+  ];
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      IconButton(onPressed: (){
-        query ='';
-      }, icon: const Icon(Icons.clear))
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear)
+      )
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return
-      IconButton(onPressed: (){
+    return IconButton(
+      onPressed: () {
         close(context, null);
-      }, icon: const Icon(Icons.arrow_back))
-    ;
+      },
+      icon: const Icon(Icons.arrow_back)
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    List<String> matchQuery=[];
-    for(var element in elements){
-      String work = element;
-      if(work.toLowerCase().contains(query.toLowerCase())){
-        matchQuery.add(element);
-      }
-    }
+    List<String> matchQuery = searchItems
+        .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
     return ListView(
       padding: const EdgeInsets.symmetric(horizontal: 20).w,
       children: [
         Text(
-          '${matchQuery.length} result:',
+          '${matchQuery.length} results:',
           style: AppFonts.font23,
           textAlign: TextAlign.start,
         ),
+        SizedBox(height: 20.h),
         ListView.builder(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
           itemCount: matchQuery.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Text(
-              matchQuery[index],
-              style: AppFonts.font23,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                matchQuery[index],
+                style: AppFonts.font23,
+              ),
+              leading: const Icon(Icons.search),
+              onTap: () {
+                // Handle the search result tap
+                Navigator.pop(context, matchQuery[index]);
+              },
             );
           },
         ),
@@ -56,35 +73,39 @@ class CustomSearch extends SearchDelegate{
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery=[];
-    for(var element in elements){
-      String work = element;
-      if(work.toLowerCase().contains(query.toLowerCase())){
-        matchQuery.add(element);
-      }
-    }
+    List<String> matchQuery = searchItems
+        .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
     return ListView(
       padding: const EdgeInsets.symmetric(horizontal: 20).w,
       children: [
         Text(
-          'most Popular Searches',
+          query.isEmpty ? 'Recent Searches' : 'Suggestions',
           style: AppFonts.font23,
           textAlign: TextAlign.start,
         ),
-        SizedBox(height: 20.h,),
+        SizedBox(height: 20.h),
         ListView.builder(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
           itemCount: matchQuery.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Text(
-              matchQuery[index],
-              style: AppFonts.font23,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                matchQuery[index],
+                style: AppFonts.font23,
+              ),
+              leading: const Icon(Icons.history),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                query = matchQuery[index];
+                showResults(context);
+              },
             );
           },
         ),
       ],
     );
   }
-
 }
